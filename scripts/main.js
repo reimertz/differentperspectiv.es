727cf93df6a0e21d6f96de4ff9a2c427328984bd
Pierre Reimertz <pierre@pierrereimertz.com>
2015-09-24 02:28:23 +0200

    Update 2015-09-24T00:28:21.540Z

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+/* jshint devel:true */
+"use strict";
+
+function addDoughnut(){
+  var data = [
+    {
+        value: 1,
+        color:"white",
+        highlight: "rgba(255,255,255,0.5)",
+        label: "Yes"
+    },
+    {
+        value: 9,
+        color: "transparent",
+        highlight: "rgba(255,255,255,0.5)",
+        label: "No"
+    }
+  ];
+
+  var options = {
+      maintainAspectRatio: true,
+      segmentStrokeWidth : 1,
+     showTooltips: false
+  }
+
+  new Chart(document.getElementById('1-out-of-10').getContext("2d")).Pie(data,options);
+}
+
+var waypoint = new Waypoint({
+  element: document.getElementById('1-out-of-10'),
+  handler: function(direction) {
+    setTimeout(addDoughnut, 1000);
+    this.destroy();
+  },
+  offset: '50%'
+})
+
+
+Array.prototype.slice.call(document.querySelectorAll('main > section')).map(function(section){
+  new Waypoint({
+    element: section,
+    handler: function(direction) {
+      this.element.classList.add('seen');
+      this.destroy();
+    },
+    offset: '40%'
+  })
+});
