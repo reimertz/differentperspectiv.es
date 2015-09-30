55b59f8a853046f5fb4c1e6f2aa299552e91f6b5
Pierre Reimertz <pierre@pierrereimertz.com>
2015-09-30 19:42:20 +0200

    Update 2015-09-30T17:38:45.489Z

--- scripts/main.js
+++ scripts/main.js
@@ -46,3 +46,27 @@ Array.prototype.slice.call(document.querySelectorAll('main > section')).map(func
     offset: '40%'
   })
 });
+
+
+
+Array.prototype.slice.call(document.querySelectorAll('[data-iphone-step]')).map(function(solutionStep){
+  new Waypoint({
+    element: solutionStep,
+    handler: function(direction) {
+      var method = (direction === 'down') ? 'add' : 'remove',
+          step = this.element.getAttribute('data-iphone-step');
+
+      document
+        .querySelector('.solution-fixed')
+        .classList[method](step);
+
+        if(step === 'vr') {
+          var method = (direction === 'down') ? 'play' : 'pause';
+
+          document.querySelector('[data-vr-video]').currentTime = 0;
+          document.querySelector('[data-vr-video]')[method]();
+        }
+    },
+    offset: '30%'
+  })
+});
